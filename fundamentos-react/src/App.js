586e3a4f13005024626e4c0a0942f5484465de3e
Cadu <cadumega@outlook.com>
2021-09-03 12:05:30 -0300

    Estilização com StyledComponents

--- fundamentos-react/src/App.js
+++ fundamentos-react/src/App.js
@@ -1,10 +1,33 @@
+//npm install styled-components --save
+// Itens estruturais usamos stled components
+// Depois de estruturar, crio os componentes em variaveis para estarem declaradas.
 
 import React from "react";
+import styled from 'styled-components';
+
+const Site = styled.div `
+width:400px;
+height:400px;
+background-color:#00FF;
+`;
+
+const Botao = styled.button `
+font-size:19px;
+padding:10px 15px;
+`;
+
+const Title = styled.h1 `
+    color:#FF0;
+    font-size:18px;
+`;
 
 function App() {
-    return <>
-
-    </>
+    return (
+        <Site>
+            <Title>Título do site</Title>
+            <Botao>Clique aqui</Botao>
+        </Site>
+    );
 }
 
 export default App;
