ba466857b38c9965b8792d0e8b7100cc420e8857
Cadu <cadumega@outlook.com>
2021-09-08 23:53:09 -0300

    useEffect

--- fundamentos-react/src/app.js
+++ fundamentos-react/src/app.js
@@ -1,61 +1,33 @@
-// Calculadora de gorjeta.
-// Exibição condicional, usando duas states com a conta e outra que tem a porcentagem
-// 2 campos inputs type number
-// ao digitar vem como string, depois transformar para number.
+//Ciclo de vida de um componente UseEffect, lidar com efeitos, com as etapadas do ciclo de vida.
+
+import react, {useState, useEffect} from 'React';
+import styled from 'styled-components';
+
+function App() {
+
+    const[contagem, setContagem] = UseState(0);
+
+    useEffect(()=> {
+        document.title='Contagem: '+contagem;
+    }, [contagem])
+
+    function aumentarAction () {
+        setContagem(contagem +1);                    //incremento 
+    }
 
 
-import React,{useState} from "react";
-import styled from "styled-components";
 
-const Title = styled.h1`
-  font-family: Arial;
-  color: royalblue;
-`;
-
-const Input = styled.input`
-  width: 200px;
-  height: 20px;
-  border: 1px solid #333;
-  border-radius: 5px;
-  padding: 10px;
-  font-size: 1.1rem;
-`;
-
-let App = () => {
-  const [conta, setConta] = useState(0);
-  const [percent, setPercent] = useState(10);
-
-  const gorjeta = Number((percent / 100) * conta);
   return (
     <>
-      <Title>Calculadora de Gorjeta</Title>
-
-      <p>Quanto deu a conta?</p>
-      <Input
-        type="number"
-        value={conta}
-        onChange={e => setConta(e.target.value)}
-      />
-
-      <p>Qual a percentagem de gorjeta?</p>
-      <Input
-        type="number"
-        value={percent}
-        onChange={e => setPercent(e.target.value)}
-      />
-      <hr />
-
-      {conta > 0 && (
-        <div>
-          <p>Sub-total: R$ {conta}</p>
-          <p>
-            Gorjeta({percent}%): R$ {gorjeta};
-          </p>
-          <p>Total: R$ {Number(conta) + gorjeta} </p>
-        </div>
-      )}
+        <h1>Contagem: {contagem}</h1>
+        <button onClick={aumentarAction}>Aumentar Número</button>
+    
     </>
   );
-};
+}
 
 export default App;
+
+
+
+//DocumentTitle para mudar título
